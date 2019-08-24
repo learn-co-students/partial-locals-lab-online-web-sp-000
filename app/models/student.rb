a60b2c3154db4dbae2710b52a6db5774962b016d
Ramon Chavez <rachavez1974@gmail.com>
2019-08-24 17:17:45 -0400

    Finish partial-locals-lab-online-web-sp-000

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,15 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(student_key)
+    if student_key.empty?
+      self.all
+    else
+      self.all.collect do |student|
+        student if student.name.downcase.include?(student_key) 
+      end.compact
+    end
+  end
+
 end
