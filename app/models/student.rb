1231204ac1b7c31022346fbbcb9583f1f9b4e468
Kenton Archer <kenton8654@yahoo.com>
2019-08-27 18:55:06 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
+
 # == Schema Information
 #
 # Table name: students
@@ -13,4 +14,22 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+    # look for all students whos name begin with search and then return it.
+    # if no name is given return all students
+    if name == ''
+      # if (name) = 'string'
+      return Student.all
+      # return an array of all student objects or
+      else
+      Student.all.select do |s|
+        # look thru the array of students and select
+        # students name which is lower-cased and includes part of the string var = (name) 
+        s.name.downcase.include? name
+      end
+    end
+  end
+
+
 end
